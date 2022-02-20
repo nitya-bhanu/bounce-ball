e30772539a3828f4326a24d3b31af0377875682b
nitya-bhanu <89220459+nitya-bhanu@users.noreply.github.com>
2022-02-20 11:17:51 +0530

    Added audio properties

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var animation;
 document.getElementById("bl").addEventListener("click", function () {
   anime({
     targets: ".ball",
-    bottom:300,
+    bottom: 300,
     direction: "alternate",
     easing: "linear",
     duration: "400",
@@ -15,7 +15,7 @@ var animation1 = anime({
   duration: "4000",
   loop: true,
   easing: "linear",
-  autoplay: false
+  autoplay: false,
 });
 var animation2 = anime({
   targets: ".movingobstacles2",
@@ -23,67 +23,90 @@ var animation2 = anime({
   duration: "5500",
   loop: true,
   easing: "linear",
-  autoplay: false
+  autoplay: false,
 });
 var score = anime({
-    targets: '.score',
-    autoplay: false,
-    innerHTML: "High Score: "+[6000],
-    duration: 40000,
-    easing: 'linear',
-    round: 1
-  });
-document.querySelector('.pause').addEventListener('click', function () {
-    animation1.pause();
-    animation2.pause();
-    score.pause();
-  })
-  document.querySelector('.play').addEventListener('click', function () {
-    animation1.play();
-    animation2.play();
-    score.play();
-  })
-  document.querySelector('.restart').addEventListener('click', function () {
-    animation1.restart();
-    animation2.restart();
-    score.restart();
-    document.getElementById("msg").innerHTML="";
-  })
+  targets: ".score",
+  autoplay: false,
+  innerHTML: "High Score: " + [6000],
+  duration: 40000,
+  easing: "linear",
+  round: 1,
+});
 
-  var update = 0;
-  var el = document.getElementById('time');
-  
-  function incrementScore() {
-      update += 1;
-      el.innerText = "TIme Elapsed: " + update + " seconds.";
-  }
-  var cancel = setInterval(incrementScore, 1000);
 
+/********IMPLEMENTING SOUNDS************/
+let playaudioSound = new Audio(
+  "Play Game.mp3"
+);
+let endGameSound=new Audio(
+  "Game Over.mp3"
+)
+
+document.querySelector(".pause").addEventListener("click", function () {
+  animation1.pause();
+  animation2.pause();
+  score.pause();
+  playaudioSound.pause()
+});
+document.querySelector(".play").addEventListener("click", function () {
+  animation1.play();
+  animation2.play();
+  score.play();
+  playaudioSound.play();
+});
+document.querySelector(".restart").addEventListener("click", function () {
+  animation1.restart();
+  animation2.restart();
+  score.restart();
+  playaudioSound.play();
+  document.getElementById("msg").innerHTML = "";
+});
+
+var update = 0;
+var el = document.getElementById("time");
+
+function incrementScore() {
+  update += 1;
+  el.innerText = "You have been here for " + update + " seconds.";
+}
+var cancel = setInterval(incrementScore, 1000);
 
 window.setInterval(getOffsets, 10);
 function getOffsets() {
   var ballpos = document.querySelector(".ball");
   var box1pos = document.querySelector(".movingobstacles");
   var box2pos = document.querySelector(".movingobstacles2");
-  var topball=ballpos.offsetTop;
-  var topbox1=box1pos.offsetTop;
-  var topbox2=box2pos.offsetTop;
+  var topball = ballpos.offsetTop;
+  var topbox1 = box1pos.offsetTop;
+  var topbox2 = box2pos.offsetTop;
   if (
-    (box1pos.offsetLeft+box1pos.offsetWidth >ballpos.offsetLeft && box1pos.offsetLeft+box1pos.offsetWidth <=ballpos.offsetLeft+ballpos.offsetWidth )&&
-    topbox1-topball<=ballpos.offsetHeight
+    box1pos.offsetLeft + box1pos.offsetWidth > ballpos.offsetLeft &&
+    box1pos.offsetLeft + box1pos.offsetWidth <=
+      ballpos.offsetLeft + ballpos.offsetWidth &&
+    topbox1 - topball <= ballpos.offsetHeight
   ) {
     // document.querySelector('.movingobstacles') = animation1.pause
     // document.querySelector('.movingobstacles2') = animation2.pause;
     // const x=update;
-    document.querySelector('.pause').click();
-    document.getElementById("msg").innerHTML="Game Over";
+    endGameSound.play();
+    // endGameSound.loop=false;
+    document.querySelector(".pause").click();
+    document.getElementById("msg").innerHTML = "Game Over";
   } else if (
-    (box2pos.offsetLeft+box2pos.offsetWidth >=ballpos.offsetLeft && box2pos.offsetLeft+box2pos.offsetWidth <=ballpos.offsetLeft+ballpos.offsetWidth ) &&
-    topbox2-topball<=ballpos.offsetHeight
+    box2pos.offsetLeft + box2pos.offsetWidth >= ballpos.offsetLeft &&
+    box2pos.offsetLeft + box2pos.offsetWidth <=
+      ballpos.offsetLeft + ballpos.offsetWidth &&
+    topbox2 - topball <= ballpos.offsetHeight
   ) {
     // document.querySelector('.movingobstacles') = animation1.pause;
     // document.querySelector('.movingobstacles2') = animation2.pause;
-    document.querySelector('.pause').click();
-    document.getElementById("msg").innerHTML="Game Over";
+    endGameSound.play();
+    // endGameSound.loop=false;
+    document.querySelector(".pause").click();
+    document.getElementById("msg").innerHTML = "Game Over";
   }
 }
+
+
+
